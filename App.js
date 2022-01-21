73997c8b1329306774d0c982e12872175e3bd73f
Eli Zibin <elizibin@gmail.com>
2022-01-21 12:48:25 -0800

    Expo Custom font with Victory Native
    This uses Montserrat as an example with a few charts
    to show that it should be possible to use an expo loaded
    custom font with Victory Native.

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryTheme,
+  VictoryLabel,
+  VictoryAxis,
+} from "victory-native";
+
+import { useFonts } from "expo-font";
+
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
 
 export default function App() {
+  const [loaded] = useFonts({
+    Montserrat: require("./assets/fonts/Montserrat.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryBar
+          data={data}
+          x="quarter"
+          y="earnings"
+          style={{
+            labels: {
+              fill: "red",
+              fontSize: 25,
+              fontFamily: "Montserrat",
+            },
+          }}
+          labels={({ datum }) => `y: ${datum.earnings}`}
+          labelComponent={<VictoryLabel dy={30} />}
+        />
+      </VictoryChart>
+      <VictoryChart>
+        <VictoryBar
+          style={{
+            data: { fill: "tomato", width: 25, fontFamily: "Montserrat" },
+            labels: { fontFamily: "Montserrat" },
+          }}
+          data={[
+            { x: "cats", y: 10 },
+            { x: "dog", y: 25 },
+            { x: "bird", y: 40 },
+            { x: "frog", y: 50 },
+            { x: "fish", y: 50 },
+          ]}
+        />
+        <VictoryAxis style={{ tickLabels: { fontFamily: "Montserrat" } }} />
+        <VictoryAxis
+          dependentAxis
+          style={{ tickLabels: { fontFamily: "Montserrat" } }}
+        />
+      </VictoryChart>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff",
   },
 });
